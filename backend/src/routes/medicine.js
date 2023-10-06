0e86a46d7e27faf77a0763d06e78dd3d77964803
Nour Alansay <nouralansay@Gihans-MacBook-Air.local>
2023-10-06 15:56:39 +0300

    updated medicine routes

--- backend/src/routes/medicine.js
+++ backend/src/routes/medicine.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const Medicine = require('../models/Medicines');
+
+const { default: mongoose } = require('mongoose');
 
 const router = express.Router();
 
 //get all medicine
-router.get('/' , (req,res)=>{
-    res.json({mssg : 'get all medicine'})
+router.get('/' , async (req,res)=>{
+    try{
+        console.log("ana hena");
+        const medicine = await Medicine.find()
+        console.log("ana hena");
+        console.log(medicine);
+        res.status(200).json(medicine)
+
+    }catch (error){
+        res.status(400).json({error : error.message})
+
+    }
+    //res.json({mssg : 'get all medicine'})
 })
 //get a specific medicine by name
 router.get('/:name' , (req,res)=>{
