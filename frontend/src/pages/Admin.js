dbf0f77c70872d2b512fcfe7a0ff9608101472bd
Mohamed Shamekh <shamekhjr@gmail.com>
2023-10-13 21:32:51 +0300

    Added two new pages in admin branch

---
+++ frontend/src/pages/Admin.js
@@ -0,0 +1,9 @@
+const Admin = () => {
+  return (
+    <div className="admin">
+      <h2>This page will be to view admins and create new admins</h2>
+    </div>
+  );
+};
+
+export default Admin;
---
+++ frontend/src/pages/MedicinesForAdmin.js
@@ -0,0 +1,9 @@
+const MedicinesForAdmin = () => {
+  return (
+    <div className="medicinesForAdmin">
+      <h2>this page will be to view and filter medicines</h2>
+    </div>
+  );
+};
+
+export default MedicinesForAdmin;
