68b96ac181b77d33b1dcfec650baeb99e25526a2
Mohamed Shamekh <shamekhjr@gmail.com>
2023-10-14 00:44:38 +0300

    seeinf medicines and searching and filtering now works in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Admin from "./pages/Admin";
+import MedicinesForAdmin from "./pages/MedicinesForAdmin";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/admin" element={<Admin />} />
+        <Route exact path="/admin/medicines" element={<MedicinesForAdmin />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/useAxios.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import React from "react";
+
+const useAxios = (baseUrl) => {
+  const [medicines, setMedicines] = React.useState([]);
+
+  React.useEffect(() => {
+    axios
+      .get(baseUrl)
+      .then((response) => {
+        console.log(response.data);
+        setMedicines(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return { medicines };
+};
+
+export default useAxios;
