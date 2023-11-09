ed52143c0e844c5128950d5f2ef0a57f8c94873f
Nour Alansary <nouralansary24@gmail.com>
2023-11-09 10:55:42 +0200

    test

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 
 const Cart = () => {
+  
+  
   const [cartItems, setCartItems] = useState([]);
 
   useEffect(() => {
@@ -25,29 +27,86 @@ const Cart = () => {
       } else {
         throw new Error('Network response was not ok');
       }})
-      .then((data) => setCartItems(data))
+      .then((data) => setCartItems(data.data.medicines))
       .catch((error) => console.error('Error fetching cart items:', error));
+
+
+
   }, []);
-// const fetchMedicines = async () => {
-//     try {
-//       console.log("HERE:");
-//       const response = await fetch("http://localhost:8000/medicine");
-//       console.log("Response status:", response.status);
 
-//       if (!response.ok) {
-//         throw new Error("Failed to fetch data");
-//       }
-//       const json = await response.json();
-//       console.log("Fetched data:", json);
+  const [medicines, setMedicines] = useState([]);
 
-//       // Store the fetched data in the 'data' state
-//       setCartItems(json);
-//     } catch (error) {
-//       console.error("Error fetching data:", error);
-//     }
+
+  useEffect(() => {
+  const fetchMedicines = (cartItems) => {
+    // Send a request to your server with the list of medicine IDs
+    fetch('http://localhost:8000/medicine/getArrayMedicinesByID', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ cartItems }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Network response was not ok');
+        }
+      })
+      .then((medicineOutputs) => {
+        // Here, you have the specific medicines, and you can update your component state accordingly
+        console.log('Medicines:', medicines);
+        setMedicines(medicineOutputs);
+      })
+      .catch((error) => console.error('Error fetching medicines:', error));
+  };
+  fetchMedicines(cartItems);
+
+}, [cartItems]);
+
+// // const fetchMedicines = async () => {
+// //     try {
+// //       console.log("HERE:");
+// //       const response = await fetch("http://localhost:8000/medicine");
+// //       console.log("Response status:", response.status);
+
+// //       if (!response.ok) {
+// //         throw new Error("Failed to fetch data");
+// //       }
+// //       const json = await response.json();
+// //       console.log("Fetched data:", json);
+
+// //       // Store the fetched data in the 'data' state
+// //       setCartItems(json);
+// //     } catch (error) {
+// //       console.error("Error fetching data:", error);
+// //     }
+// //   };
+// const fetchMedicines = (medicineIds) => {
+//     // Send a request to your server with the list of medicine IDs
+//     fetch('http://localhost:8000/medicines', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({ medicineIds }),
+//     })
+//       .then((response) => {
+//         if (response.ok) {
+//           return response.json();
+//         } else {
+//           throw new Error('Network response was not ok');
+//         }
+//       })
+//       .then((medicines) => {
+//         // Here, you have the specific medicines, and you can update your component state accordingly
+//         console.log('Medicines:', medicines);
+//       })
+//       .catch((error) => console.error('Error fetching medicines:', error));
 //   };
-console.log(cartItems);
-
+// console.log(cartItems);
+  var i = 0;
   return (
     <div>
       <h2>Cart Items</h2>
@@ -64,14 +123,14 @@ console.log(cartItems);
           </tr>
         </thead>
         <tbody>
-          {cartItems&cartItems.map((item) => (
+          {medicines&&medicines.map((item) => (
             <tr key={item.id}>
               <td>{item.medicineName}</td>
               <td>{item.description}</td>
               <td>{item.medicinalUsage}</td>
               <td>{item.price}</td>
-              <td>{item.quantity}</td>
-              <td>{item.quantity*item.price}</td>
+              <td>{cartItems[i].quantity}</td>
+              <td>{cartItems[i++].quantity*item.price}</td>
               <td>
                 <img src={item.photo} alt={item.name} width="100" />
               </td>
