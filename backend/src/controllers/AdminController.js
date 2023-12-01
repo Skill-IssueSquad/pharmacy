fe6a14348e2d3cc9ef8c7582ca053f82ad29514b
youssefalaa398 <119411027+youssefalaa398@users.noreply.github.com>
2023-12-01 14:44:51 +0200

    .

--- backend/src/controllers/AdminController.js
+++ backend/src/controllers/AdminController.js
@@ -293,6 +293,8 @@ const viewPharmacistRequests = async (req, res) => {
 
 const getAllOrders = async (req, res) => {
   try {
+    const selectedDate = req.query.date;
+
     // Find all patients and populate the 'orders' field with necessary fields
     const patients = await Patient.find()
       .populate({
@@ -302,25 +304,44 @@ const getAllOrders = async (req, res) => {
       })
       .select('orders.date orders.cart.medicines');
 
-    // Flatten the nested structure
-    const allOrders = patients.map(patient => patient.orders).flat();
+    // Create a map to store the aggregated information for each medicine on a specific day
+    const medicinesInfoMap = new Map();
 
-    // Extract relevant information for each medicine
-    const medicinesInfo = [];
-    allOrders.forEach(order => {
-      order.cart.medicines.forEach(medicine => {
-        if (medicine.medicine_id) {
-          const medicineInfo = {
-            medicine_id: medicine.medicine_id._id || medicine.medicine_id,
-            medicineName: medicine.medicine_id.medicineName,
-            quantity: medicine.quantity,
-            totalPrice: calculateTotalPrice(medicine.quantity, medicine.medicine_id.price),
-            date: order.date,
-          };
-          medicinesInfo.push(medicineInfo);
+    // Process each patient's orders
+    patients.forEach((patient) => {
+      patient.orders.forEach((order) => {
+        const orderDate = new Date(order.date).toLocaleDateString();
+
+        // Check if the order is for the selected date
+        if (!selectedDate || orderDate === selectedDate) {
+          // Process each medicine in the order
+          order.cart.medicines.forEach((medicine) => {
+            if (medicine.medicine_id) {
+              const medicineKey = `${medicine.medicine_id._id || medicine.medicine_id}_${orderDate}`;
+
+              if (!medicinesInfoMap.has(medicineKey)) {
+                // Initialize the map entry for the medicine on the specific day
+                medicinesInfoMap.set(medicineKey, {
+                  medicine_id: medicine.medicine_id._id || medicine.medicine_id,
+                  medicineName: medicine.medicine_id.medicineName,
+                  quantity: 0,
+                  totalPrice: 0,
+                  date: orderDate,
+                });
+              }
+
+              // Update quantity and total price for the medicine on the specific day
+              const medicineInfo = medicinesInfoMap.get(medicineKey);
+              medicineInfo.quantity += medicine.quantity;
+              medicineInfo.totalPrice += calculateTotalPrice(medicine.quantity, medicine.medicine_id.price);
+            }
+          });
         }
       });
     });
+
+    // Convert the map values to an array
+    const medicinesInfo = Array.from(medicinesInfoMap.values());
 
     res.json(medicinesInfo);
   } catch (error) {
@@ -328,7 +349,6 @@ const getAllOrders = async (req, res) => {
     res.status(500).json({ error: 'Internal server error' });
   }
 };
-
 // Function to calculate total price for each medicine
 function calculateTotalPrice(quantity, unitPrice) {
   return quantity * unitPrice;
