772f8f443ee0fa5176b310734424342f586aa44b
youssefalaa398 <119411027+youssefalaa398@users.noreply.github.com>
2023-12-03 02:36:49 +0200

    MAIL OUT OF STOCK HANDLED BY MY MAIL, NEED TO FETCH THE PHARMACIST MAILS INSTEAD OF MINE

--- backend/src/controllers/patientController.js
+++ backend/src/controllers/patientController.js
@@ -3,6 +3,7 @@ const Medicine = require("../models/Medicines");
 const Patient = require("../models/Patient");
 
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
 
 const getCart = async (req,res) =>{
     const { username } = req.body;
@@ -128,71 +129,84 @@ const addAddressToPatient = async (req,res) =>{
 }
 
 
-const addOrderToPatient = async (req,res) =>{
-    //console.log("ana in");
-    const { username,status,date,cart,discount,netPrice,deliveryAddress,paymentMethod } = req.body;
-    try{
-        
-        const patient = await Patient.findOne({username:username})
-        if(patient == null){
-            return res.status(404).json({success: false , message: "Patient not found" , data: null})
-        }
-        else{
-            //console.log("ana in address");
+const addOrderToPatient = async (req, res) => {
+  const { username, status, date, cart, discount, netPrice, deliveryAddress, paymentMethod } = req.body;
 
-              const newAddress= {
-                streetName: deliveryAddress.streetName,
-                propertyNum: deliveryAddress.propertyNumber,
-                floorNum: deliveryAddress.floorNumber,
-                apartNum: deliveryAddress.apartmentNumber,
-                extraLandMarks: deliveryAddress.extraLandmarks,
+  try {
+      const patient = await Patient.findOne({ username: username });
+
+      if (patient == null) {
+          return res.status(404).json({ success: false, message: "Patient not found", data: null });
+      } else {
+          const newAddress = {
+              streetName: deliveryAddress.streetName,
+              propertyNum: deliveryAddress.propertyNumber,
+              floorNum: deliveryAddress.floorNumber,
+              apartNum: deliveryAddress.apartmentNumber,
+              extraLandMarks: deliveryAddress.extraLandmarks,
+          };
+
+          const cart1 = {
+              medicines: cart,
+          };
+
+          const order = {
+              status: status,
+              date: date,
+              cart: cart1,
+              discount: discount,
+              netPrice: netPrice,
+              deliveryAddress: newAddress,
+              paymentMethod: paymentMethod,
+          };
+
+          patient.orders.push(order);
+          await patient.save();
+
+          for (let i = 0; i < cart.length; i++) {
+              const medicine = await Medicine.findOne({ _id: cart[i].medicine_id });
+              const oldQuantity = medicine.quantity;
+              medicine.quantity = medicine.quantity - cart[i].quantity < 0 ? 0 : medicine.quantity - cart[i].quantity;
+              await medicine.save();
+
+              // Check if the medicine is out of stock and send an email to the pharmacist
+              if (medicine.quantity === 0 && oldQuantity > 0) {
+                  const transporter = nodemailer.createTransport({
+                      service: "gmail",
+                      auth: {
+                          user: "el7a2ni.virtual@gmail.com",
+                          pass: "zijy ztiz drcn ioxq",
+                      },
+                  });
+
+                  const mailOptions = {
+                      from: "el7a2ni.virtual@gmail.com",
+                      to: "youssefalaa398@gmail.com",
+                      subject: "Medicine Out of Stock",
+                      text: `The medicine ${medicine.medicineName} is out of stock. Please update the inventory.`,
+                  };
+
+                  transporter.sendMail(mailOptions, function (error, info) {
+                      if (error) {
+                          console.log(error);
+                      } else {
+                          console.log("Email sent: " + info.response);
+                      }
+                  });
               }
+          }
 
-              const cart1= {
-                medicines : cart
-              }
-             
-
-
-              const order = {
-                status: status,
-                date: date,
-                cart : cart1,
-                discount: discount,
-                netPrice: netPrice,
-                deliveryAddress : newAddress,
-                paymentMethod: paymentMethod
-
-              };
-
-           patient.orders.push(order);
-           await patient.save();
-
-
-           for(let i = 0;i<cart.length;i++){
-            const medicine = await Medicine.findOne({_id:cart[i].medicine_id});
-            medicine.quantity = medicine.quantity - cart[i].quantity < 0 ? 0 : medicine.quantity - cart[i].quantity;
-            await medicine.save();
-           }
-
-           return res.status(200).json({success: true , message: "Patient  found" , data: patient})
-
-           
-           
-        }
-    }
-    catch (error) {
-        const reply = {
+          return res.status(200).json({ success: true, message: "Patient found", data: patient });
+      }
+  } catch (error) {
+      const reply = {
           success: false,
           data: null,
           message: error.message,
-        };
-        return res.status(500).json(reply);
-      }
-
-}
-
-
+      };
+      return res.status(500).json(reply);
+  }
+};
 
 const deleteOrder = async (req,res)=>{
 
