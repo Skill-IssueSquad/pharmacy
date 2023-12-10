eadd5583661e263a6485cae705c31a636f840ec6
Nour Alansary <nouralansary24@gmail.com>
2023-12-10 14:00:59 +0200

    some improvements in gui

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,43 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Button, Container } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { useState,useEffect } from 'react';
+import Box from '@mui/material/Box';
+
 
 const Navbar = () => {
+
+
+const username ="fewf";
+
+const [walletBalance, setWalletBalance] = useState(0);
+
+
+const getWalletBalance = (username) => {
+  fetch('http://localhost:8001/pharmacist/medicines/getWalletBalance', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username }),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json(); // Return the JSON response
+      } else {
+        throw new Error('Network response was not ok');
+      }
+    })
+    .then((data) => {
+      console.log(data.data.walletBalance);
+      //walletBalance = data.data.walletBalance;
+
+      setWalletBalance(data.data);
+    })
+    .catch((error) => console.error('Error fetching wallet balance:', error));
+};
+
+getWalletBalance(username);
   return (
     <AppBar position="static">
       <Container>
@@ -20,6 +55,7 @@ const Navbar = () => {
           >
             Sales
           </Button>
+          <Box>
           <Button
             component={Link}
             to="/pharmacist/medicines/addmedicine"
@@ -27,6 +63,14 @@ const Navbar = () => {
           >
             AddMedicine
           </Button>
+          </Box>
+          <Box >
+            <div style={{ marginLeft: '20px', marginBottom: '12px' }}>
+            <Typography variant="body1" style={{ marginTop: '10px', marginRight: '10px' }}>
+              Wallet Balance: {walletBalance}
+            </Typography>
+        </div>
+          </Box>
          
         </Toolbar>
       </Container>
---
+++ frontend/src/components/navBarP.js
@@ -0,0 +1,216 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import { useNavigate } from 'react-router-dom';
+import { useState,useEffect } from 'react';
+
+
+const pages = ['Home', 'Cart', 'Orders','Change Password'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const username ="fewf";
+
+
+function ResponsiveAppBar() {
+let navigate = useNavigate();
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const [walletBalance, setWalletBalance] = useState(0);
+
+
+const getWalletBalance = (username) => {
+  fetch('http://localhost:8001/pharmacist/medicines/getWalletBalance', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username }),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json(); // Return the JSON response
+      } else {
+        throw new Error('Network response was not ok');
+      }
+    })
+    .then((data) => {
+      console.log(data.data.walletBalance);
+      //walletBalance = data.data.walletBalance;
+
+      setWalletBalance(data.data);
+    })
+    .catch((error) => console.error('Error fetching wallet balance:', error));
+};
+
+getWalletBalance(username);
+
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Patient Dashboard
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Patient Dashboard
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => {
+                let url = "/patient/"
+                if (page === "Home") {
+                    url = "/pharmacist/medicines"
+                } else if (page === "Cart") {
+                    url = "/Cart/"
+                } else if (page === "Orders") {   
+                    url = "/orderDetails/"
+                }
+                else if (page === "Change Password") {
+                    url = "/medicinePatient/"
+                }
+                return(
+              <Button
+                key={page}
+                onClick={() => navigate(url)}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>)
+})}
+          </Box>
+          <Box >
+            <div style={{ marginLeft: '20px', marginTop: '10px' }}>
+            <Typography variant="body1" style={{ marginTop: '10px', marginRight: '10px' }}>
+              Wallet Balance: {walletBalance}
+            </Typography>
+        </div>
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default ResponsiveAppBar;
