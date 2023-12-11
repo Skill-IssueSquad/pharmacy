942a4d1f5ac9a791f9e1f8d3dcc3207db4476da8
WaleedElgamal <waleedelgamall3@gmail.com>
2023-12-11 18:57:56 +0200

    [Development] added pharmacist request page

--- backend/src/controllers/PharmacistRequestController.js
+++ backend/src/controllers/PharmacistRequestController.js
@@ -1,7 +1,6 @@
 const PharmacistRequest = require("../models/PharmacistRequest");
 
-
-//View doctor request info 
+//View pharmacist request info 
 const viewInfo = async (req,res) => {
     try{
         const username = req.body.username;
@@ -23,33 +22,8 @@ const viewInfo = async (req,res) => {
 };
 
 
-//Update doctor request info
-const updateInfo = async (req,res) => { 
-    const condition = {username: req.params.username};
-    const updateData = req.body;
-    console.log(updateData);
-    console.log(condition);
-    const update = { $set: updateData};
-    PharmacistRequest.updateOne(condition, update)
-     .then(() => {
-        const reply = {
-            success: true,
-            data: req.params.username,
-            message: "Pharmacist request updated successfully",
-        };
-        res.status(200).json(reply);
-     })
-     .catch((error) => {
-        const reply = {
-            success: false,
-            data: null,
-            message: error.message,
-        };
-        res.status(400).json(reply);
-     })
-};
+
 
 module.exports = {
     viewInfo,
-    updateInfo
 };
